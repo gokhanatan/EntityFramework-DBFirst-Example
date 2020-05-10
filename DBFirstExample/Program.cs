ed3357e7df657e030561dd9b32cf34656eeabb38
gkhnatn <gokhan.atan@ordinatrum.com.tr>
2020-05-10 03:23:41 +0300

    EntitiyFramework projenin oluşturuldu.Model eklendi.Ve ilk data çekilme işlemi yapıldı.

---
+++ DBFirstExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NorthwindEntities context = new NorthwindEntities();
+            var categories = context.Categories.ToList();
+            foreach (var item in categories)
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
