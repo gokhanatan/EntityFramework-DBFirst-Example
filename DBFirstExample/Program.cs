ef3be6c16db7bb410b4a4a48490a64e6d63a56a6
gkhnatn <gokhan.atan@ordinatrum.com.tr>
2020-05-10 04:33:24 +0300

    EntityFramework ekleme,silme ve güncelleme metot örnekleri eklendi.

--- DBFirstExample/Program.cs
+++ DBFirstExample/Program.cs
@@ -10,16 +10,60 @@ namespace DBFirstExample
     {
         static void Main(string[] args)
         {
+            //ReadExample();
+
+            //AddExample();
+
+            //UpdateExample();
+
+            DeleteExample();
+
+            Console.ReadLine();
+
+        }
+
+        private static void DeleteExample()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var category = context.Categories.Find(9);
+                context.Categories.Remove(category);
+                context.SaveChanges();
+
+            }
+        }
+
+        private static void UpdateExample()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var category = context.Categories.Find(9);
+
+                category.CategoryName = "Spor";
+                category.Description = "Bu bir spor kategorisidir.";
+
+                context.SaveChanges();
+            }
+        }
+
+        private static void AddExample()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                Category category = new Category() { CategoryName = "Bilişim", Description = "Bu bir bilişim kategorisidir." };
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        private static void ReadExample()
+        {
             NorthwindEntities context = new NorthwindEntities();
             var categories = context.Categories.ToList();
             foreach (var item in categories)
             {
                 Console.WriteLine(item.CategoryName);
             }
-
-            Console.ReadLine();
-
         }
-
     }
 }
