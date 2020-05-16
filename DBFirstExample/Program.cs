40750d78bdb593a239469dd36ab7eac7d214901e
gkhnatn <gokhan.atan@ordinatrum.com.tr>
2020-05-16 16:55:19 +0300

    Entity Framework Linq ile basit sorgu oluşturma örnekleri eklendi.

--- DBFirstExample/Program.cs
+++ DBFirstExample/Program.cs
@@ -10,16 +10,116 @@ namespace DBFirstExample
     {
         static void Main(string[] args)
         {
+            //LinqSimpleQuery5();
+
+            //LinqSimpleQuery4();
+
+            //LinqSimpleQuery3();
+
+            //LinqSimpleQuery2();
+
+            //LinqSimpleQuery1();
+
+
             //ReadExample();
 
             //AddExample();
 
             //UpdateExample();
 
-            DeleteExample();
+            //DeleteExample();
 
             Console.ReadLine();
 
+        }
+
+        private static void LinqSimpleQuery5()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var products = context.Products.Where(p => p.UnitPrice > 100 ||
+                p.SupplierID == 3);
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine("ÜrünAdı: " + product.ProductName +
+                        " Fiyatı: " + product.UnitPrice +
+                        " SuplierID: " + product.SupplierID);
+                }
+            }
+        }
+
+        private static void LinqSimpleQuery4()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var products = context.Products.Where(p => p.ProductName.Contains("Chef") ||
+                    p.Discontinued != false);
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine("ProductName: " + product.ProductName +
+                        " İndirimDurumu: " + product.Discontinued);
+                }
+
+            }
+        }
+
+        private static void LinqSimpleQuery3()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var products = context.Products.Where(p => p.UnitsInStock > 100 &&
+                p.QuantityPerUnit.Contains("bottles"));
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine("QuantityPerUnit: " + product.QuantityPerUnit +
+                        " StokMiktarı: " + product.UnitsInStock);
+                }
+
+            }
+        }
+
+        private static void LinqSimpleQuery2()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                //var products = context.Products.Select(p => new
+                //{
+                //    UrunAdi = p.ProductName,
+                //    Fiyati = p.UnitPrice
+                //});
+
+                var products = from prod in context.Products
+                               select new
+                               {
+                                   UrunAdi = prod.ProductName,
+                                   Fiyati = prod.UnitPrice
+                               };
+
+                foreach (var item in products)
+                {
+                    Console.WriteLine("UrunAdi: " + item.UrunAdi +
+                        " Fiyatı: " + item.Fiyati);
+                }
+
+            }
+        }
+
+        private static void LinqSimpleQuery1()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                //var products = context.Products;
+                var products = context.Products.ToList();
+
+                foreach (var product in products)
+                {
+                    Console.WriteLine("ProductID: " + product.ProductID +
+                        "ProductName: " + product.ProductName);
+                }
+            }
         }
 
         private static void DeleteExample()
